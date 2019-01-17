2e2b43f9a3d1423a4187bd83bdb8709ce7a88dd3
Gregor <gregor@martynus.net>
2019-01-17 14:17:20 -0800

    test: `log` client option

--- test/integration/smoke-test.js
+++ test/integration/smoke-test.js
@@ -10,7 +10,7 @@ describe('smoke', () => {
     Octokit()
   })
 
-  it('host & pathPrefix options', () => {
+  it('baseUrl options', () => {
     nock('http://myhost.com')
       .get('/my/api/orgs/myorg')
       .reply(200, {})
@@ -173,5 +173,46 @@ describe('smoke', () => {
     expect(octokit.issues.get).to.be.a('function')
 
     return octokit.issues.fooBar()
+  })
+
+  it('options.log', () => {
+    // console.debug not implemented in Node 6
+    // cy.stub(console, 'debug')
+    cy.stub(console, 'info')
+    cy.stub(console, 'warn')
+    cy.stub(console, 'error')
+    const octokit1 = new Octokit()
+
+    octokit1.log.debug('foo')
+    octokit1.log.info('bar')
+    octokit1.log.warn('baz')
+    octokit1.log.error('daz')
+
+    // expect(console.debug.callCount).to.equal(0)
+    expect(console.info.callCount).to.equal(0)
+    expect(console.warn.callCount).to.equal(1)
+    expect(console.error.callCount).to.equal(1)
+
+    const calls2 = []
+    const octokit2 = new Octokit({
+      log: {
+        debug: calls2.push.bind(calls2),
+        info: calls2.push.bind(calls2),
+        warn: calls2.push.bind(calls2),
+        error: calls2.push.bind(calls2)
+      }
+    })
+
+    octokit2.log.debug('foo')
+    octokit2.log.info('bar')
+    octokit2.log.warn('baz')
+    octokit2.log.error('daz')
+
+    expect(calls2).to.deep.equal([
+      'foo',
+      'bar',
+      'baz',
+      'daz'
+    ])
   })
 })
